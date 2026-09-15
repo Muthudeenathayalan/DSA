85d9eb8a66839a368600f74d3ba3f156448daccf
Muthudeenathayalan V <muthudeenathayalan369@gmail.com>
2026-09-15 19:57:22 +0530

    Solve Add two values (5/5 tests passed)

---
+++ placement-path/variables/problem-02/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        long a=input.nextInt();
+        long b=input.nextInt();
+        System.out.println(a+b);
+        
+    }
+}
