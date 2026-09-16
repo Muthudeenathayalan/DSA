bcf0fb6c99349510244cfb7a12b3d891b3f4ed8a
Muthudeenathayalan V <muthudeenathayalan369@gmail.com>
2026-09-17 00:52:43 +0530

    Solve Compare two ratios (5/5 tests passed)

---
+++ placement-path/operators/problem-08/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Read the input, implement your approach, and print the answer.
+        int a=input.nextInt();
+        int b=input.nextInt();
+        int c=input.nextInt();
+        int d=input.nextInt();
+        System.out.print((a*d)==(c*b));
+    }
+}
