612301b4ea9014fb7a81af0114d665b787fc8391
Muthudeenathayalan V <muthudeenathayalan369@gmail.com>
2026-09-16 11:06:05 +0530

    Solve Quotient and remainder (5/5 tests passed)

---
+++ placement-path/variables/problem-07/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Read the input, implement your approach, and print the answer.
+        int a=input.nextInt();
+        int b=input.nextInt();
+        int c=a/b;
+        int d=a%b;
+        System.out.print(c+" "+d);
+    }
+}
