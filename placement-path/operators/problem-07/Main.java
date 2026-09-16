9fcda37ff0cf84b13df938336cac7eb3ebec52bb
Muthudeenathayalan V <muthudeenathayalan369@gmail.com>
2026-09-17 00:41:36 +0530

    Solve Percentage of an amount (5/5 tests passed)

---
+++ placement-path/operators/problem-07/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Read the input, implement your approach, and print the answer.
+        int a=input.nextInt();
+        int b=input.nextInt();
+        double p=(a*b)/100.0;
+        System.out.print(p);
+    }
+}
