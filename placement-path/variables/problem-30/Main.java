1607423a5a425dee8440579b20ddaba99ae0456d
Muthudeenathayalan V <muthudeenathayalan369@gmail.com>
2026-09-17 23:40:59 +0530

    Solve Two-currency wallet (6/6 tests passed)

---
+++ placement-path/variables/problem-30/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Read the input, implement your approach, and print the answer.
+        int r=input.nextInt();
+        int d=input.nextInt();
+        int x=input.nextInt();
+        System.out.print(r+(d*x));
+    }
+}
