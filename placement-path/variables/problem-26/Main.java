e4e8626317aef9c839c5b5315ff816a4c0a7a3ec
Muthudeenathayalan V <muthudeenathayalan369@gmail.com>
2026-09-17 22:12:33 +0530

    Solve Circle measurements (6/6 tests passed)

---
+++ placement-path/variables/problem-26/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Read the input, implement your approach, and print the answer.
+        double pi=3.141592653589793;
+        int r=input.nextInt();
+        System.out.print(pi*r*r+" "+2*pi*r);
+    }
+}
