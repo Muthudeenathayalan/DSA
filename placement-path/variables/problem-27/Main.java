6ab67b4d051cff1349d918dc8f9c4c7bd142017b
Muthudeenathayalan V <muthudeenathayalan369@gmail.com>
2026-09-17 22:19:23 +0530

    Solve Celsius and Fahrenheit (6/6 tests passed)

---
+++ placement-path/variables/problem-27/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Read the input, implement your approach, and print the answer.
+        int a=input.nextInt();
+        System.out.print((double)((9*a)/5.0)+32.0);
+    }
+}
