97a2173c85017b0e39fa17daa5ae36561f1e267a
Muthudeenathayalan V <muthudeenathayalan369@gmail.com>
2026-09-18 14:40:25 +0530

    Solve Extract the last two digits (6/6 tests passed)

--- placement-path/operators/problem-26/Main.java
+++ placement-path/operators/problem-26/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner input = new Scanner(System.in);
         // Read the input, implement your approach, and print the answer.
         int a=input.nextInt();
-        int b=(a/10)%10;
+        int b=(a/10);
         int c=a%10;
         System.out.print(b+" "+c);
     }
