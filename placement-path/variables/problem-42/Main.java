8e1667fbc24c28e95eb120b63f3b0cb025b690d7
Muthudeenathayalan V <muthudeenathayalan369@gmail.com>
2026-09-18 11:44:14 +0530

    Solve Token allowance after requests (6/6 tests passed)

---
+++ placement-path/variables/problem-42/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Read the input, implement your approach, and print the answer.
+        int a=input.nextInt();
+        int n=input.nextInt();
+        int i=input.nextInt();
+        int o=input.nextInt();
+        System.out.print(a-(n*(i+o)));
+    }
+}
