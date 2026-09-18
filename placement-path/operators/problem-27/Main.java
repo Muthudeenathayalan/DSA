d507d23f2d0c973c31177bb12afd0340e0023c39
Muthudeenathayalan V <muthudeenathayalan369@gmail.com>
2026-09-18 14:34:42 +0530

    Solve Swap the final two digits (6/6 tests passed)

---
+++ placement-path/operators/problem-27/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Read the input, implement your approach, and print the answer.
+        int a=input.nextInt();
+        int b=(a/10);
+        int c=a%10;
+        System.out.print(c+""+b);
+        
+    }
+}
