7f367eba4c78593657469cd2527294d5444a1a54
Muthudeenathayalan V <muthudeenathayalan369@gmail.com>
2026-09-18 09:52:29 +0530

    Solve Clock after a lesson (6/6 tests passed)

---
+++ placement-path/variables/problem-34/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Read the input, implement your approach, and print the answer.
+        int a=input.nextInt();
+        int b=input.nextInt();
+        int c=input.nextInt();
+        System.out.print(a+" "+(b+c));
+    }
+}
