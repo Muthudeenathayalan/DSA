70cbc5cd09aa564fe1f2ba8fbd9685595c0aaa8f
Muthudeenathayalan V <muthudeenathayalan369@gmail.com>
2026-09-18 15:46:15 +0530

    Solve Exclusive divisibility (6/6 tests passed)

---
+++ placement-path/operators/problem-30/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Read the input, implement your approach, and print the answer.
+        int a=input.nextInt();
+        int b=input.nextInt();
+        int c=input.nextInt();
+        boolean result=((a%b==0)^(a%c==0));
+        System.out.print(result);
+    }
+}
